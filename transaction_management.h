602689764b6f565994ebf7e9c165ecd4de1e9345
Hirwa Walid <155954891+Hirw23@users.noreply.github.com>
2025-06-06 15:21:13 +0200

    Update transaction_management.h

--- transaction_management.h
+++ transaction_management.h
@@ -1,11 +1,21 @@
+// transaction_management.h
+
 #ifndef TRANSACTION_MANAGEMENT_H
 #define TRANSACTION_MANAGEMENT_H
 
-#include "user_management.h"
+#define MAX_TRANSACTIONS 100
+#define MAX_TRANSACTION_LENGTH 100
 
+struct User {
+    float balance;
+    char transaction_log[MAX_TRANSACTIONS][MAX_TRANSACTION_LENGTH];
+    int transaction_count;
+};
+
+// Declare the functions if needed elsewhere
 void check_balance(struct User *current_user);
 void deposit(struct User *current_user);
 void withdraw(struct User *current_user);
 void view_transactions(struct User *current_user);
 
-#endif 
+#endif
